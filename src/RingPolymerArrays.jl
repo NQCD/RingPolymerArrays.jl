ef2713bdfdc132e685f0f5b7d89e1c8e8823404f
James Gardner <james.gardner1421@gmail.com>
2022-04-23 13:34:18 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/RingPolymerArrays.jl
@@ -0,0 +1,5 @@
+module RingPolymerArrays
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RingPolymerArrays
+using Test
+
+@testset "RingPolymerArrays.jl" begin
+    # Write your tests here.
+end

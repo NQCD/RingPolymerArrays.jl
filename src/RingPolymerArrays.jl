f4c4b1c99afa6e36d6e9d5965647e02489ce4a5b
James Gardner <james.gardner1421@gmail.com>
2022-04-25 14:58:53 +0100

    Add exports

--- src/RingPolymerArrays.jl
+++ src/RingPolymerArrays.jl
@@ -24,5 +24,8 @@ export eachbead
 export eachdof
 
 include("normal_mode_transformation.jl")
+export NormalModeTransformation
+export transform_to_normal_modes!
+export transform_from_normal_modes!
 
 end # module
